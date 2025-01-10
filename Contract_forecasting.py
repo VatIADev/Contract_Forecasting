45c4da7ff9b37f5b9ae04091a20e355d7ecc6073
VatIADev <iadevsvatia@gmail.com>
2025-01-10 15:58:53 -0500

    Update Contract_forecasting.py

--- Contract_forecasting.py
+++ Contract_forecasting.py
@@ -164,7 +164,7 @@ def main():
 
         .st-key-cont-load, .st-key-cont-result{
             background-color:#396425ff !important;
-            padding: 0.1em;
+            padding: 0.4em;
             border-radius: 20px;
         }
         
