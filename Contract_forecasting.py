249e7fc13a32bc9f2d5e77f60c77e68ba43efc02
VatIADev <iadevsvatia@gmail.com>
2025-01-10 16:03:57 -0500

    Update Contract_forecasting.py

--- Contract_forecasting.py
+++ Contract_forecasting.py
@@ -106,10 +106,10 @@ def totales(datos,modelo,t_c,std):
 
 def graficar(resultados):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=resultados['ds'], y=resultados['p_max'], mode='lines+markers', marker=dict(size=10,color='rgba (9, 90, 129, 1)', opacity=0.8),
-                             line=dict(width=3, color='rgba (9, 90, 129, 1)', dash='dash'), name='', showlegend=False))
-    fig.add_trace(go.Scatter(x=resultados['ds'], y=resultados['p_min'], mode='lines+markers', marker=dict(size=10,color='rgba (9, 90, 129, 1)', opacity=0.8),
-                             fill='tonexty', line=dict(width=3, color='rgba (9, 90, 129, 1)', dash='dash'), name='Precio Contratos', fillcolor='rgba(173, 216, 230, 0.5)'))
+    fig.add_trace(go.Scatter(x=resultados['ds'], y=resultados['p_max'], mode='lines+markers', marker=dict(size=10,color='rgba (174, 234, 0, 1)', opacity=0.8),
+                             line=dict(width=3, color='rgba (174, 234, 0, 1)', dash='dash'), name='', showlegend=False))
+    fig.add_trace(go.Scatter(x=resultados['ds'], y=resultados['p_min'], mode='lines+markers', marker=dict(size=10,color='rgba (174, 234, 0, 1)', opacity=0.8),
+                             fill='tonexty', line=dict(width=3, color='rgba (174, 234, 0, 1)', dash='dash'), name='Precio Contratos', fillcolor='rgba(9, 90, 129, 0.5)'))
     fig.update_xaxes(showline=True, linewidth=2, linecolor='black', gridcolor='lightgray', mirror=False, tickfont_color='black',
                      title_text='<b>Año</b>', titlefont_size=18, tickfont_size=16, title_font_color='black',)
     fig.update_yaxes(showline=True, linewidth=2, linecolor='black', gridcolor='lightgray', mirror=False, tickfont_color='black',
