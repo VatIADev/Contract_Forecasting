0dea9dcd8ade63d7f357d79b6d2bcb6dfea2f20b
VatIADev <iadevsvatia@gmail.com>
2025-01-10 16:31:19 -0500

    Update Contract_forecasting.py

--- Contract_forecasting.py
+++ Contract_forecasting.py
@@ -172,7 +172,7 @@ def estilo():
                 font-size: 1.3em;
             }
 
-            .st-key-cont-variables{
+            .st-key-cont-variables p{
                 background-color:#396425ff !important;
             }
 
