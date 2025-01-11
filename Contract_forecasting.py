c86a448814a7fa006ac72b8388404804ac35f8de
VatIADev <iadevsvatia@gmail.com>
2025-01-11 16:40:12 -0500

    Update Contract_forecasting.py

--- Contract_forecasting.py
+++ Contract_forecasting.py
@@ -111,11 +111,11 @@ def graficar(resultados):
     fig.add_trace(go.Scatter(x=resultados['ds'], y=resultados['p_min'], mode='lines+markers', marker=dict(size=10,color='rgba (174, 234, 0, 1)', opacity=0.8),
                              fill='tonexty', line=dict(width=3, color='rgba (174, 234, 0, 1)', dash='dash'), name='Precio Contratos', fillcolor='rgba(9, 90, 129, 0.5)'))
     fig.update_xaxes(showline=True, linewidth=2, linecolor='white', gridcolor='lightgray', mirror=False, tickfont_color='white',
-                     title_text='<b>📆 Año</b>', titlefont_size=18, tickformat="%b %Y", tickfont_size=16, title_font_color='white',)
+                     title_text='<b>Año</b>', titlefont_size=18, tickformat="%b %Y", tickfont_size=16, title_font_color='white',)
     fig.update_yaxes(showline=True, linewidth=2, linecolor='white', gridcolor='lightgray', mirror=False, tickfont_color='white',
-                     title_text='<b>💲Precio (COP/kWh)</b>', titlefont_size=18, tickfont_size=16, title_font_color='white',
+                     title_text='<b>Precio (COP/kWh)</b>', titlefont_size=18, tickfont_size=16, title_font_color='white',
                      tickformat='.2f')
-    fig.update_traces(hovertemplate='📆 Año:</b> %{x}<br><b>💲Precio Contrato:</b> %{y:.2f} COP/kWh<extra></extra>')
+    fig.update_traces(hovertemplate='📆 Año:</b> %{x}<br><b>💲Precio Unitario Contrato:</b> %{y:.2f} COP/kWh<extra></extra>')
     fig.update_layout(title='', plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
                       font=dict(family="Prompt", color='white'),
                       legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=0.98,
